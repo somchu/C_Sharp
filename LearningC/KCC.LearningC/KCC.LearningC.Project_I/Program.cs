9eae5a05684927e5a76a95b90133e224b904230d
somchu <somchu29@gmail.com>
2020-06-16 00:12:59 +0700

    commit branch1

--- LearningC/KCC.LearningC/KCC.LearningC.Project_I/Program.cs
+++ LearningC/KCC.LearningC/KCC.LearningC.Project_I/Program.cs
@@ -9,8 +9,28 @@ namespace KCC.LearningC.Project_I
     {
         static void Main(string[] args)
         {
+            int num1 = 0;
+            int num2 = 0;
+            int answer = 0;
+
             Console.WriteLine("Hello World!!!");
             Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            string sNum1 = Console.ReadLine();
+
+            Console.Write("Enter another number: ");
+            string sNum2 = Console.ReadLine();
+
+            //TryParse สำหรับ check ค่า ตัวแปร ถ้าภูกต้องจะไปลงในตัวแปร
+            Int32.TryParse(sNum1, out num1);
+            Int32.TryParse(sNum2, out num2);
+
+            answer = num1 + num2;
+            Console.WriteLine(answer);
+            
+            Console.ReadLine();
+          
         }
     }
 }
